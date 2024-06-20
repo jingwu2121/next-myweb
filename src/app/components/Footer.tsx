7b7246b396d6fc5d7285138ece7a1893506ba98e
jingwu2121 <jing.wu10.21@gmail.com>
2024-06-20 19:50:47 +0100

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div>
         <div className={`${style.taskbar} text-center text-xs text-xs md:text-sm`}>
-            Copyright 2024 @ Wu Jing | All Rights Reserved | CSS from Alex B
+            Copyright 2024 @ Wu Jing | All Rights Reserved | Theme from Alex B | Written in Next.js
         </div>
     </div>
   )
